f6834fc8ac2e0185e861e8c1c78d09c34048c785
Ilya <45985532+moloko22@users.noreply.github.com>
2020-01-17 11:24:23 +0300

    add styles to Onecard

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+import { DatePicker, InputNumber } from 'antd';
+import moment from 'moment';
+import 'antd/dist/antd.css';
 import './Card.css';
-function Card(props){
-    const onClickHandle = () =>{
+const dateFormat = 'YYYY/MM/DD';
+
+class Card extends Component {
+
+    onClickHandle = () =>{
         console.log('Заказано!')
     };
-    return(
-        <div className={'one_card_wrapper'}>
-            <div className={'one_card'}>
-                <div className={'one_card_image'}>
-                    <img src={`${props.card.img}`} alt=""/>
-                </div>
-                <div className={'one_card_info'}>
-                    <h3>{props.card.header}</h3>
-                    <p className={'about'}>{props.card.text}</p>
-                    <p className={'about'}>Компания:{props.card.company}</p>
-                    <p className={'about'}>Год выпуска оборудования:{props.card.year}</p>
-                    <p className={'about'}>Возможна доставка с {props.card.date}</p>
-                    <button onClick={onClickHandle}>Заказать</button>
-                    <p className={'price'}>{props.card.price} руб.</p>
+    onChange(value) {
+        console.log('changed', value);
+    }
+    render() {
+        return (
+            <div className={'one_card_wrapper'}>
+                <div className={'one_card'}>
+                    <div className={'one_card_images'}>
+                        <img src={`${this.props.card.img}`} alt=""/>
+                    </div>
+                    <div className={'one_card_info'}>
+                        <h3>{this.props.card.header}</h3>
+                        <h4>{this.props.card.city}, {this.props.card.company}</h4>
+                        <p>{this.props.card.text}</p>
+                    </div>
+                    <div className={'one_card_form'}>
+                        <p><span>{this.props.card.price} руб.</span> за сутки</p>
+                        <form action="">
+                            <label htmlFor="date_from">Начало аренды</label>
+                            <DatePicker defaultValue={moment('2020/01/01', dateFormat)} format={dateFormat} />
+                            <label htmlFor="date_to">Конец аренды</label>
+                            <DatePicker defaultValue={moment('2020/01/01', dateFormat)} format={dateFormat} />
+                            <label htmlFor="quantity">Количество</label>
+                            <InputNumber min={1} max={100} defaultValue={3} onChange={this.onChange} />
+                            <button onClick={this.onClickHandle}>Отправить заявку</button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
 export default Card;
