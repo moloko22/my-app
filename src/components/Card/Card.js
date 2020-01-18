ca3e71fad81c43006c23bcdd48a31832419cb289
Ilya <45985532+moloko22@users.noreply.github.com>
2020-01-18 14:53:32 +0300

    change code of filter Date

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,101 +12,80 @@ class Card extends Component {
         this.props.orderRequest(this.props.card);
     };
     changeDateFormat(array){
-        console.log(array);
+        array.splice(0, 1);
         let obj = [];
-        if(array[0] === 'Mon' || array[0] === 1){
-            obj[0] = 'Понедельник'
+        if(array[0] === 0 || array[0] === 'Jan') {
+            obj[0] = 'Января'
         }
-        if(array[0] === 'Tue' || array[0] === 2){
-            obj[0] = 'Вторник'
+        if(array[0] === 1 || array[0] === 'Feb'){
+            obj[0] = 'Февраля'
         }
-        if(array[0] === 'Wed' || array[0] === 3){
-            obj[0] = 'Среда'
+        if(array[0] === 2 || array[0] === 'Mar'){
+            obj[0] = 'Марта'
         }
-        if(array[0] === 'Thu' || array[0] === 4){
-            obj[0] = 'Четверг'
+        if(array[0] === 3 || array[0] === 'Apr'){
+            obj[0] = 'Апреля'
         }
-        if(array[0] === 'Fri' || array[0] === 5){
-            obj[0] = 'Пятница'
+        if(array[0] === 4 || array[0] === 'May'){
+            obj[0] = 'Мая'
         }
-        if(array[0] === 'Sut' || array[0] === 6){
-            obj[0] = 'Суббота'
+        if(array[0] === 5 || array[0] === 'Jun'){
+            obj[0] = 'Июня'
         }
-        if(array[0] === 'Sun' || array[0] === 7){
-            obj[0] = 'Воскресенье'
-        }
-        if(array[1] === 0 || array[1] === 'Jan') {
-            obj[1] = 'Января'
-        }
-        if(array[1] === 1 || array[1] === 'Feb'){
-            obj[1] = 'Февраля'
-        }
-        if(array[1] === 2 || array[1] === 'Mar'){
-            obj[1] = 'Марта'
-        }
-        if(array[1] === 3 || array[1] === 'Apr'){
-            obj[1] = 'Апреля'
-        }
-        if(array[1] === 4 || array[1] === 'May'){
-            obj[1] = 'Мая'
-        }
-        if(array[1] === 5 || array[1] === 'Jun'){
-            obj[1] = 'Июня'
-        }
-        if(array[1] === 6 || array[1] === 'Jul'){
-            obj[1] = 'Июля'
+        if(array[0] === 6 || array[0] === 'Jul'){
+            obj[0] = 'Июля'
         }
 
-        if(array[1] === 7 || array[1] === 'Aug'){
-            obj[1] = 'Августа'
+        if(array[0] === 7 || array[0] === 'Aug'){
+            obj[0] = 'Августа'
         }
 
-        if(array[1] === 8 || array[1] === 'Sep'){
-            obj[1] = 'Сентября'
+        if(array[0] === 8 || array[0] === 'Sep'){
+            obj[0] = 'Сентября'
         }
-        if(array[1] === 9 || array[1] === 'Oct'){
-            obj[1] = 'Октрября'
+        if(array[0] === 9 || array[0] === 'Oct'){
+            obj[0] = 'Октрября'
         }
-        if(array[1] === 10 || array[1] === 'Nov'){
-            obj[1] = 'Ноября'
+        if(array[0] === 10 || array[0] === 'Nov'){
+            obj[0] = 'Ноября'
         }
-        if(array[1] === 11 || array[1] === 'Dec'){
-            obj[1] = 'Декарбря'
+        if(array[0] === 11 || array[0] === 'Dec'){
+            obj[0] = 'Декарбря'
         }
-        if(array[2] === '01'){
-            obj[2] = 1;
+        if(array[1] === '01'){
+            obj[1] = 1;
         }
-        if(array[2] === '02'){
-            obj[2] = 2;
+        if(array[1] === '02'){
+            obj[1] = 2;
         }
-        if(array[2] === '03'){
-            obj[2] = 3;
+        if(array[1] === '03'){
+            obj[1] = 3;
         }
-        if(array[2] === '04'){
-            obj[2] = 4;
+        if(array[1] === '04'){
+            obj[1] = 4;
         }
-        if(array[2] === '05'){
-            obj[2] = 5;
+        if(array[1] === '05'){
+            obj[1] = 5;
         }
-        if(array[2] === '06'){
-            obj[2] = 6;
+        if(array[1] === '06'){
+            obj[1] = 6;
         }
-        if(array[2] === '07'){
-            obj[2] = 7;
+        if(array[1] === '07'){
+            obj[1] = 7;
         }
-        if(array[2] === '08'){
-            obj[2] = 8;
+        if(array[1] === '08'){
+            obj[1] = 8;
         }
-        if(array[2] === '09'){
-            obj[2] = 9;
+        if(array[1] === '09'){
+            obj[1] = 9;
         }
-        if(typeof array[2] === 'number' && array[2] > 9 && array[2] < 32){
-            obj[2] = array[2];
+        if(typeof array[1] === 'number' && array[1] > 9 && array[1] < 32){
+            obj[1] = array[1];
         }
-        if(typeof array[3] === 'string'){
-            obj[3] = +array[3];
+        if(typeof array[2] === 'string'){
+            obj[2] = +array[2];
         }else{
-            obj[3] = array[3];
+            obj[2] = array[2];
         }
         return obj;
     }
@@ -117,10 +96,12 @@ class Card extends Component {
             this.props.card[name] = this.changeDateFormat(value._d.toString().split(' ', 4));
             this.props.card.dateOrder = this.changeDateFormat([orderDate.getDay(), orderDate.getMonth(), orderDate.getDate(), orderDate.getFullYear()]);
             this.props.card.quantity = 1;
-            console.log(this.props.card);
         }
         if(name === 'quantity'){
             this.props.card.quantity = value;
+        }
+        if(name === 'comment'){
+            this.props.card.comment = value;
         }
     }
     render() {
@@ -138,22 +119,26 @@ class Card extends Component {
                     <div className={'one_card_form'}>
                         <p><span>{this.props.card.price} руб.</span> за сутки</p>
                         <form action={''}>
-                            <label htmlFor="date_from">Начало аренды</label>
+                            <label htmlFor="date_from">Начало аренды:</label>
                             <DatePicker name={'dataFrom'}
                                         onChange={(e) => this.onChange('dateFrom', e)}
                                         defaultValue={moment('2020.01.01', dateFormat)}
                                         format={dateFormat} />
-                            <label htmlFor="date_to">Конец аренды</label>
+                            <label htmlFor="date_to">Конец аренды:</label>
                             <DatePicker onChange={(e) => this.onChange('dateTo', e)}
                                         name={'dataFrom'}
                                         defaultValue={moment('2020.01.01', dateFormat)}
                                         format={dateFormat} />
-                            <label htmlFor="quantity">Количество</label>
+                            <label htmlFor="quantity">Количество:</label>
                             <InputNumber min={1}
                                          name={'quantity'}
                                          max={100}
                                          defaultValue={1}
                                          onChange={(e) =>this.onChange('quantity', e)} />
+                            <label htmlFor="comment">Комментарий:</label>
+                            <textarea id={'comment'}
+                                      onChange={(e) => this.onChange('comment', e.target.value)}
+                            />
                             <button onClick={(e) => this.onClickHandle(e)}>Отправить заявку</button>
                         </form>
                     </div>
